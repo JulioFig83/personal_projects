6eea9b6acc90a90f96972dfba9c86878d71368e2
Filo PC <julio.figueroa@kcl.ac.uk>
2020-06-15 13:35:48 +0100

    added sine function

--- github_practice.py
+++ github_practice.py
@@ -1,9 +1,17 @@
-# -*- coding: utf-8 -*-
+
 """
 Practice code for GitHub
 
 """
+import numpy as np
+import math
+import matplotlib.pyplot as plt
 
 x = "hello world"
+y = np.linspace(0,math.pi * 2)
+z = np.sin(y)
+
+plt.plot(y,z)
+
 
 print(x)
