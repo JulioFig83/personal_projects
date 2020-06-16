90e2f27e55a0582b1aad542ae585c5e6dd7212bc
Filo PC <julio.figueroa@kcl.ac.uk>
2020-06-16 19:09:33 +0100

    Task 01, Bohr radius

---
+++ PHY667/Task_01.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 16 19:02:52 2020
+
+@author: Julio
+"""
+
+'''Task 1 - Scientific Computing'''
+
+# We want to use Python to calculate a physical
+# quantity: Bohr Radius
+
+import numpy as np
+
+# setting up parameters
+
+epsilon_0 = 8.85e-12
+hbar      = 6.63e-34 / (2*np.pi)
+mass_e    = 9.11e-31
+charge_e  = 1.6e-19
+
+# Bohr radius
+
+a_0 = 4*np.pi * epsilon_0 * hbar**2 / (mass_e * charge_e**2)
+
+print("Bohr radius is: ", a_0)
