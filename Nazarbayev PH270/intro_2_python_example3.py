52ece924f3a360d5f56d4f834ef323185d231099
Filo PC <julio.figueroa@kcl.ac.uk>
2020-06-22 19:52:56 +0100

    password code simple

--- Nazarbayev PH270/intro_2_python_example3.py
+++ Nazarbayev PH270/intro_2_python_example3.py
@@ -5,3 +5,23 @@ Created on Sat Jun 20 17:07:29 2020
 @author: Julio
 """
 
+'''Third example, login information'''
+
+# We want to create a program and put information 
+# inside the program that a USER want to retrieve
+
+# the USER needs a password
+# If password is incorrect, deny 'entry' and ask again
+
+p = input("Enter password: ")
+password = "python"
+
+while p != password:
+    p = input("Password is wrong, try again: ")
+    
+print("\n Password: Correct ")
+print("\n Here is the top secret information.")
+
+# This will ask again and again until is correct.
+
+
