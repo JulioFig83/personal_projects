b01e257e4fb5984df74a509ef0fb9f070ee16bd8
Filo PC <julio.figueroa@kcl.ac.uk>
2020-06-22 19:59:22 +0100

    3 attempt password

--- Nazarbayev PH270/intro_2_python_example3.py
+++ Nazarbayev PH270/intro_2_python_example3.py
@@ -13,14 +13,26 @@ Created on Sat Jun 20 17:07:29 2020
 # the USER needs a password
 # If password is incorrect, deny 'entry' and ask again
 
-p = input("Enter password: ")
-password = "python"
+p           = input("Enter password: ")
+password    = "python"
+max_trial   = 3
+
+trial       = 1
 
 while p != password:
     p = input("Password is wrong, try again: ")
+    trial = trial +1
     
-print("\n Password: Correct ")
-print("\n Here is the top secret information.")
+    if trial == max_trial:
+        break 
+
+if trial == max_trial:
+    print("\n You exceeded the max. number of trials.")
+    print("\n Please contact Support Team.")
+
+else:   
+    print("\n Password: Correct ")
+    print("\n Here is the top secret information.")
 
 # This will ask again and again until is correct.
 
