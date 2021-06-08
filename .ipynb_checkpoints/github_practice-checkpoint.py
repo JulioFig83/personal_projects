26c2d365e8f3a15255db19978ffc95511057cc9f
Filo PC <julio.figueroa@kcl.ac.uk>
2021-06-08 20:10:31 +0100

    starting 2021

---
+++ .ipynb_checkpoints/github_practice-checkpoint.py
@@ -0,0 +1,17 @@
+
+"""
+Practice code for GitHub
+
+"""
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+x = "hello world"
+y = np.linspace(0,math.pi * 2)
+z = np.sin(y)
+
+plt.plot(y,z)
+
+
+print(x)
